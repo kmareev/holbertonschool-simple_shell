b2e6cd915480fa4c8f7e79c35a8a5e9ac6b24b7b
‘Krisha <8805@holbertonstudents.com>
2024-04-17 13:17:37 +0000

    Displays the shell prompt to user

--- prompt.c
+++ prompt.c
@@ -1,7 +1,7 @@
 #include "simple_shell.h"
 
 /**
- * display_prompt - Displays the shell prompt to user
+ * display_prompt - Displays the shell prompt to user to type a command.
  *
  */
 void display_prompt(void)
