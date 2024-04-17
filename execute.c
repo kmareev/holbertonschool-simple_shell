b490c17ee7c2ba05cf83a951a8f21c9941d35edf
‘Krisha <8805@holbertonstudents.com>
2024-04-17 13:40:22 +0000

    Executes a shell command

--- execute.c
+++ execute.c
@@ -1,40 +1,55 @@
 #include "simple_shell.h"
+#include <stdlib.h>
+#include <unistd.h> 
+
+/* External declaration for environ */
+extern char **environ;
 
 /**
  * execute_command - Executes a shell command
- * @command: The command to be executed
+ * @command:The command to be executed
  * Return: 1 if successful, otherwise -1
  */
 int execute_command(char *command)
 {
-    if (command == NULL)
-    {
-        printf("\n");
-        exit(EXIT_SUCCESS);
-    }
-    if (command[strlen(command) - 1] == '\n')
-    {
-        command[strlen(command) - 1] = '\0';
-    }
-    pid_t pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    else if (pid == 0)
-    {
-        char *argv[] = {command, NULL};
-        if (execve(command, argv, environ) == -1)
-        {
-            perror("execve");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        int status;
-        waitpid(pid, &status, 0);
-    }
-    return (1);
+	pid_t pid;
+
+	if (command == NULL)
+	{
+		printf("\n");
+		exit(EXIT_SUCCESS);
+	}
+	if (command[strlen(command) - 1] == '\n')
+	{
+		command[strlen(command) - 1] = '\0';
+	}
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		char **argv = malloc(2 * sizeof(char *));
+		if (argv == NULL)
+		{
+			perror("malloc");
+			exit(EXIT_FAILURE);
+		}
+		argv[0] = command;
+		argv[1] = NULL;
+		if (execve(command, argv, environ) == -1)
+		{
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+		free(argv);
+	}
+	else
+	{
+		int status;
+		waitpid(pid, &status, 0);
+	}
+	return (1);
 }
