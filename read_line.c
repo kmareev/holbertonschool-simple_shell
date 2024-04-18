a675bbe8d87ea4326e72a4f3313ba70ca567153a
‘Krisha <8805@holbertonstudents.com>
2024-04-18 01:42:06 +0000

    Fixed betty errors

--- read_line.c
+++ read_line.c
@@ -7,9 +7,22 @@
  */
 char *read_line(void)
 {
-    char *line = NULL;
-    ssize_t bufsize = 0;
-    
-    getline(&line, &bufsize, stdin);
-    return (line);
+	char *line = NULL;
+	ssize_t bufsize = 0;
+
+	if (getline(&line, &line_size, stdin) == -1)
+	{
+		if (feof(stdin))
+		{
+			free(line);
+			return (NULL);
+		}
+		else
+		{
+			perror("getline");
+			free(line);
+			return (NULL);
+		}
+	}
+	return (line);
 }
