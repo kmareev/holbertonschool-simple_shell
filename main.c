84be8f697539c1c71abe2672f76a2ef9d9f0e1a9
‘Krisha <8805@holbertonstudents.com>
2024-04-20 13:18:09 +0000

    Fixed betty errors

--- main.c
+++ main.c
@@ -7,7 +7,47 @@
  */
 int main(void)
 {
-	int i = 0, turn = 0
+	int turn = 0, i = 0;
+	char *line = NULL;
+	char **args = NULL;
 
+	while (1)
+	{
+		line = read_line();
+		if (!line)
+			break;
+		args = parse_line(line, " \n\t");
+		free(line);
+		if (!args[0])
+		{
+			for (i = 0; args[i]; i++)
+				free(args[i]);
+			free(args);
+			exit(turn);
+		}
+		else if (strcmp(args[0], "env") == 0)
+		{
+			for (i = 0; args[i]; i++)
+			free(args[i]);
+			free(args);
+			print_env();
+			continue;
+		}
+		else if (strcmp(args[0], "exit") == 0 && args[1] == NULL)
+		{
+			for (i = 0; args[i]; i++)
+				free(args[i]);
+			free(args);
+			exit(turn);
+		}
+		turn = execute_command(args);
 
+		for (i = 0; args[i]; i++)
+			free(args[i]);
+		free(args);
+	}
+	free (line);
+	if (turn)
+		exit(turn);
+	return (0);
 }
