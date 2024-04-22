78471ddb661cbe2cf301124524e9a89da5e2df6e
‘Krisha <8805@holbertonstudents.com>
2024-04-22 04:45:08 +0000

    Fixed execute function

--- print_env.c
+++ print_env.c
@@ -1,22 +1,21 @@
 #include "simple_shell.h"
 
 /**
- * print_env - Prints the enivornment variables
+ * print_env - Prints the environment variables
  *
- * This function prints all environemnt variables
+ * This function prints all environment variables
  *
- * Return: Always return 0.
+ * Return: no return
  */
 void print_env(void)
 {
 	int i = 0;
-	char *str = environ[0];
+	char *env_var = environ[0];
 
-	while (str != NULL)
+	while (env_var != NULL)
 	{
-		printf("%s\n", str);
+		printf("%s\n", env_var);
 		i++;
-		str = environ[i];
+		env_var = environ[i];
 	}
-	return (0);
 }
---
+++ test_main.c
@@ -0,0 +1,55 @@
+#include "simple_shell.h"
+
+/**
+ * main - Entry point of the simple shell
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	int turn = 0, i = 0;
+	char *line = NULL;
+	char **args = NULL;
+
+	while (1)
+	{
+		display_prompt();
+
+		line = read_line();
+		if (!line)
+			break;
+		args = parse_line(line, " \n\t");
+		free(line);
+		if (!args[0])
+		{
+			for (i = 0; args[i]; i++)
+				free(args[i]);
+			free(args);
+			exit(turn);
+		}
+		else if (strcmp(args[0], "env") == 0)
+		{
+			for (i = 0; args[i]; i++)
+			free(args[i]);
+			free(args);
+			print_env();
+			continue;
+		}
+		else if (strcmp(args[0], "exit") == 0 && args[1] == NULL)
+		{
+			for (i = 0; args[i]; i++)
+				free(args[i]);
+			free(args);
+			exit(turn);
+		}
+		turn = execute_command(args);
+
+		for (i = 0; args[i]; i++)
+			free(args[i]);
+		free(args);
+	}
+	free (line);
+	if (turn)
+		exit(turn);
+	return (0);
+}
